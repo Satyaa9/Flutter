fccf95debab8d7e81ea1c2afe7c114435aedde3d
Satish <dadassatish6@gmail.com>
2025-11-29 22:54:16 +0530

    new

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,5 +1,5 @@
 // ...existing code...
-import 'package:demo/HomePage.dart';
+import 'package:demo/FormDemo.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +10,7 @@ class Demo extends StatelessWidget {
   Widget build(BuildContext context) {
     print("in demo classs build method");
     return MaterialApp(
-      home: Homepage(), // use the renamed widget
+      home: FormDemo(), // use the renamed widget
     );
   }
 }
